a7badd12ec5ed39840e9f76bc8440d9e383fd272
stophyeon <jeongjihyeon401@gmail.com>
2024-03-30 22:00:05 +0900

    서비스별 dockerfile 작성

--- Purchase/src/main/java/org/example/PurchaseApplication.java
+++ Purchase/src/main/java/org/example/PurchaseApplication.java
@@ -2,16 +2,17 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableFeignClients
 @EnableDiscoveryClient
 @RefreshScope
-public class ProductApplication {
+public class PurchaseApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProductApplication.class, args);
+        SpringApplication.run(PurchaseApplication.class, args);
     }
 }
