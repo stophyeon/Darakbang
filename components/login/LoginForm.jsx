3bcc5128b477a2e5441ebc68b4d58268b5677792
KimMinWoooo <als33396dn@gmail.com>
2024-04-23 12:25:19 +0900

    Committing local changes before merge

--- components/login/LoginForm.jsx
+++ components/login/LoginForm.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
