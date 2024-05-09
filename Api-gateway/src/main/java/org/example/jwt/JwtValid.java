37696585fa21a56c22bd8e2fb57e874ae102e07a
jihyuk0414 <dealon2580@naver.com>
2024-05-09 14:29:09 +0900

    git ignore부 config/bootstrap.yml만 적용으로 변경

---
+++ Api-gateway/src/main/java/org/example/jwt/JwtValid.java
@@ -0,0 +1,63 @@
+package org.example.jwt;
+
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import java.security.Key;
+import java.util.Base64;
+
+@Component
+@Slf4j
+public class JwtValid implements InitializingBean{
+    private final Key key;
+
+
+
+    public JwtValid(@Value("${jwt.secret}") String secretKey){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
+    public boolean validateToken(String jwt){
+        log.info("JWT 검증");
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            throw new SecurityException(e.getMessage());
+        } catch (ExpiredJwtException e) {
+            throw new ExpiredJwtException(e.getHeader(), e.getClaims(), e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            throw new UnsupportedJwtException(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public TokenUser decode(String token){
+        Base64.Decoder decoder = Base64.getUrlDecoder();
+        String[] chunks = token.split("\\.");
+        String payload = new String(decoder.decode(chunks[1]));
+        log.info(payload);
+        DecodedJWT jwt = JWT.decode(token);
+
+        String id = jwt.getSubject();
+        String role = jwt.getClaim("auth").asString();
+
+        return new TokenUser(id, role);
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+}
