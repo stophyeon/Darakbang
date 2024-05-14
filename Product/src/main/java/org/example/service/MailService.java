5118e12242fff2b882d9da43e2079ba4d97558ce
jihyuk0414 <dealon2580@naver.com>
2024-05-14 21:11:21 +0900

    이미지 생성부로 변경, 테스트 필요

--- Product/src/main/java/org/example/service/MailService.java
+++ Product/src/main/java/org/example/service/MailService.java
@@ -1,19 +1,25 @@
 package org.example.service;
 
+import jakarta.activation.DataSource;
+import jakarta.activation.URLDataSource;
 import jakarta.mail.MessagingException;
+import jakarta.mail.internet.AddressException;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.example.dto.exception.CustomMailException;
 import org.example.dto.purchase.PaymentsReq;
 import org.example.repository.ProductRepository;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
+
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -40,9 +46,10 @@ public class MailService {
                 mimeMessageHelper.setSubject("Darakbang 구매내역입니다.");
 
                 // 본문
-                String realimg = productRepository.findImageRealByProductId(paymentsReqList.get(i).getProduct_id());
-                mimeMessageHelper.setText(realimg, false);
-
+                String realImgUrl = productRepository.findImageRealByProductId(paymentsReqList.get(i).getProduct_id());
+                URL url = new URL(realImgUrl);
+                DataSource dataSource = new URLDataSource(url);
+                mimeMessageHelper.addAttachment("image.jpg", dataSource);
 
                 // 이메일 발신자 설정
                 mimeMessageHelper.setFrom(new InternetAddress("dealon2580" + "@naver.com"));
@@ -50,8 +57,8 @@ public class MailService {
                 // 이메일 보내기
                 javaMailSender.send(mimeMessage);
 
-            } catch (MessagingException e) {
-                throw new RuntimeException(e);
+            } catch (Exception e) {
+                throw new CustomMailException(); // 모든 예외를 MailException으로 처리했습니다.
             }
 
 
