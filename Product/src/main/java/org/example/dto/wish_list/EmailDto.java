f642cacd8c2ffe33229a0314a8f609589a93d979
stophyeon <jeongjihyeon401@gmail.com>
2024-06-19 15:40:06 +0900

    팔로우 조회 서비스 테스트

---
+++ Product/src/main/java/org/example/dto/wish_list/EmailDto.java
@@ -0,0 +1,10 @@
+package org.example.dto.wish_list;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class EmailDto {
+    private String email;
+}
